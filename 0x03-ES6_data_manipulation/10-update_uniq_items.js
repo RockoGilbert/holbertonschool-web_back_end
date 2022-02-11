e268000d0a46c19e3cba7a78cd6ce0a30f9d9586
RockoGilbert <evolve2122@gmail.com>
2022-02-10 22:25:58 -0600

    Wrote a function that maps items depending on quantity

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+//map it out
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
+  return map;
+}
+
