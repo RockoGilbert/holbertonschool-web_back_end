2182b66eb788b2a63b4cd1ec88f7007eb7a22c97
RockoGilbert <evolve2122@gmail.com>
2022-06-01 23:39:26 -0500

    Ran wc

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Exercise:
+Exercise
 Basic annotations - add
 """
 
@@ -8,4 +8,3 @@ Basic annotations - add
 def add(a: float, b: float) -> float:
     """ Return sum of two float """
     return a + b
-    
