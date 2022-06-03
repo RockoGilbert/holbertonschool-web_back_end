30f29749660ad34ce5837e4faeb9f9529c0097c5
RockoGilbert <evolve2122@gmail.com>
2022-06-02 21:22:00 -0500

    Wait_random times with the specified max delay

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-You will wait_random n times the
+Will wait_random n times the
 with the specified max delay
 """
 
@@ -9,17 +9,17 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """[Summary]
-    
+
     Args:
         n (int): [description]
         max_delay (int): [description]
 
     Returns:
-        List[float]: [Return the list of all the delays] 
-    
+        List[float]: [Return the list of all the delays]
+
     """
     lst = [await wait_random(max_delay) for i in range(n)]
     return sorted(lst)
-
