04552b5752c1686af5351f8df5fa8c27c1ef7a32
RockoGilbert <evolve2122@gmail.com>
2022-06-08 11:03:38 -0500

    Create a class BasicCache that inherits from BaseCaching and is a caching system:

---
+++ 0x00-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Basic cache class"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic cache class
+
+    Args:
+    BaseCaching (class): Basic cache class
+    """
+
+    def put(self, key, item):
+        """Put a key/value pair into cache
+        Args:
+            key ([type]): Key/value pair
+            item (type): Key/value pair
+        """
+        if key and item:
+            self.cache_data[key] = item
+        
+    def get(self, key):
+        """Get a key/value pair from cache
+        Args
+            key (str): Key/value pair        
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+            
