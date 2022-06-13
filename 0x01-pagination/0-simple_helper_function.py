d837cab1df46422ab199010b0e80a9d5ff9bff17
RockoGilbert <evolve2122@gmail.com>
2022-06-13 15:18:52 -0500

    The function returns a tuple of size 2 containing a start index and end index

---
+++ 0x01-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The function returns a tuple of size 2 containing a start index and end index"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Args:
+        page (int): [description]
+        page_size (int): [description]
+    
+    Returns:
+        Tuple[int, int]
+    """
+
+    start_index = (page * page_size) -page_size
+    end_index = page * page_size
+    return (start_index, end_index)
