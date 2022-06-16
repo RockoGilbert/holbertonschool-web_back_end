d58e983faab13de9595043f93b96f71e59319d86
RockoGilbert <evolve2122@gmail.com>
2022-06-16 07:34:16 -0500

    Updated the class to accept a list of strings

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env/ python3
 """ Function filter_datum that returns the log message obfuscated """
-import logging 
-import os 
-import re 
+import logging
+import os
+import re
 import mysql.connector
 from typing import List
 
@@ -17,16 +17,18 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self):
+        """Initialize"""
+        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields4455555554645455555 
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        """Format"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
