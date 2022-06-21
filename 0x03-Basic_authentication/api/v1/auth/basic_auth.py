bd4916198d25f811f528922c2b6d6fff1078a245
RockoGilbert <evolve2122@gmail.com>
2022-06-21 13:27:00 -0500

    Added Base 64 function

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,8 +5,24 @@ import base64
 from api.v1.auth.auth import Auth
 from models.user import User
 from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
     """Basic Authorization Class"""
     pass
+
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract Authorization"""
+
+        if (
+            not authorization_header or type(authorization_header) != str
+             or "Basic " not in authorization_header        
+        ):
+            return None
+        return authorization_header.split()[1]
+
+
+
+
