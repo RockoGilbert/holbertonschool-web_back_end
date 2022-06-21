bdf2b527903dbeeff7d9e00daf8fe1525f5208c3
RockoGilbert <evolve2122@gmail.com>
2022-06-21 16:54:38 -0500

    Added to imports

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Creates a basic_Auth class that inherits from Auth"""
 
-import base64
 from api.v1.auth.auth import Auth
+import base64
 from models.user import User
 from typing import TypeVar
-from models.user import User
 
 
 class BasicAuth(Auth):
@@ -20,3 +19,19 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+            base64_authorization_header: str) -> str:
+        """Decode base64 authorization header"""
+
+        if not base64_authorization_header or type(base64_authorization_header
+        ) != str:
+            return None
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = base64.b64decode(encoded)
+            decoded = decoded64.decode('utf-8')
+        except Exception:
+            return None
+
+        return decoded
