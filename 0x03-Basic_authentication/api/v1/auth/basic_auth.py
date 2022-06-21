d4d84c938ec9f92b5d5e9a9994e477b5ca87b311
RockoGilbert <evolve2122@gmail.com>
2022-06-21 09:06:17 -0500

    added pycache

---
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Creates a basic_Auth class that inherits from Auth"""
+
+import base64
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+
+class BasicAuth(Auth):
+    """Basic Auth Class"""
+    pass
