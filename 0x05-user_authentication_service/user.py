3d77c398b988de1f47e244215709f8046d00b3bc
RockoGilbert <evolve2122@gmail.com>
2022-06-28 12:35:31 -0500

    Fixing errors

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 """SQLAlchemy model for a database table"""
 
-from db import DB
-from user import User
 
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, NoResultFound
 
 Base = declarative_base()
+
 
 class User(Base):
     """User model"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=True)
+    email = Column((250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
