2330cf66ade864d9b2f0c3af69b1056703fddf22
RockoGilbert <evolve2122@gmail.com>
2022-06-29 12:10:18 -0500

    Started Flask server

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -20,39 +20,39 @@ def index():
     return jsonify({"message": "Bienvenue"}), 200
 
 
-@app.route('/users', methods=['POST'])
-def create():
-    """Creates an email if the user doesn't exist"""
+# @app.route('/users', methods=['POST'])
+# def create():
+#     """Creates an email if the user doesn't exist"""
 
-    email = request.form.get('email')
-    password = request.form.get('password')
+#     email = request.form.get('email')
+#     password = request.form.get('password')
 
-    try:
-        AUTH.create_users(email, password)
-        return jsonify({"email": email, "message": "user created"}), 200
-    except Exception:
-        return jsonify({"message": "email already registered"}), 400
-
-
-@app.route('/reset_password', methods=["PUT"])
-def update_password():
-    """Route to update password"""
-
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
-    except ValueError:
-        abort(403)
-
-    {"email": "<user email>", "message": "Password updated"}, 200
+#     try:
+#         AUTH.create_users(email, password)
+#         return jsonify({"email": email, "message": "user created"}), 200
+#     except Exception:
+#         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    """Route to log user into email"""
+# @app.route('/reset_password', methods=["PUT"])
+# def update_password():
+#     """Route to update password"""
+
+#     email = request.form.get("email")
+#     reset_token = request.form.get("reset_token")
+#     new_password = request.form.get("new_password")
+#     try:
+#         AUTH.update_password(reset_token, new_password)
+#         return jsonify({"email": email, "message": "Password updated"}), 200
+#     except ValueError:
+#         abort(403)
+
+#     {"email": "<user email>", "message": "Password updated"}, 200
+
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     """Route to log user into email"""
 
 
 if __name__ == "__main__":
