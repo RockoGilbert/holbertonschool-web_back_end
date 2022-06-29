8d8af5cd11e019a8205390636e879bcac7bca939
RockoGilbert <evolve2122@gmail.com>
2022-06-29 12:14:04 -0500

    Started Flask server

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,58 +1,65 @@
 #!/usr/bin/env python3
-"""A flask app that has a single GET route"""
+''' init flask app '''
 
+import email
+from flask import Flask, abort, jsonify, redirect, request
 from auth import Auth
-from flask import Flask, jsonify, request, abort, redirect
-from sqlalchemy.orm.exc import NoResultFound
 
 
-# Authentication
 AUTH = Auth()
-
-
-# Flask App
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """Route handler"""
-    return jsonify({"message": "Bienvenue"}), 200
+    '''init flask app '''
+    return jsonify({"message": "Bienvenue"})
 
 
-# @app.route('/users', methods=['POST'])
-# def create():
-#     """Creates an email if the user doesn't exist"""
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def registerUser():
+    '''Register a new user.
+    Args:
+        email: email address of the user
+        password: hashed password of the user
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
 
-#     email = request.form.get('email')
-#     password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
 
-#     try:
-#         AUTH.create_users(email, password)
-#         return jsonify({"email": email, "message": "user created"}), 200
-#     except Exception:
-#         return jsonify({"message": "email already registered"}), 400
-
-
-# @app.route('/reset_password', methods=["PUT"])
-# def update_password():
-#     """Route to update password"""
-
-#     email = request.form.get("email")
-#     reset_token = request.form.get("reset_token")
-#     new_password = request.form.get("new_password")
-#     try:
-#         AUTH.update_password(reset_token, new_password)
-#         return jsonify({"email": email, "message": "Password updated"}), 200
-#     except ValueError:
-#         abort(403)
-
-#     {"email": "<user email>", "message": "Password updated"}, 200
+    except ValueError:
+        return jsonify({'message': "email already registered"}), 400
 
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     """Route to log user into email"""
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Route for loggin in a user"""
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    if not (AUTH.valid_login(
+                             email=email,
+                             password=pwd)) or not email or not pwd:
+        abort(401)
+
+    session_id = AUTH.create_session(email=email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Route for logging out a user"""
+    session_id = request.cookies.get('session_id')
+    if not session_id or not AUTH.get_user_from_session_id(session_id):
+        abort(403)
+
+    AUTH.destroy_session(session_id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
