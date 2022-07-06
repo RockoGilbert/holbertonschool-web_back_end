49c2e093ca6471d1136157ab8116020a8a92d575
RockoGilbert <evolve2122@gmail.com>
2022-07-05 21:31:21 -0500

    Unit tests for utility model

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-"""Module for test_utils.py"""
+""" All unittests for utils.py module """
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Defines the TestAccessNestedMap Class"""
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
+    """Class testing utils.access_nest_map from utils.py"""
+
+    @parameterized.expand([({"a": 1}, ("a",), 1),
+                           ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                           ({"a": {"b": 2}}, ("a", "b"), 2)
+                           ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Test that utils.access_nested_map returns the expected result
-        """
+        """Tests function with three different situations"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
-    ])
+    @parameterized.expand([({}, ("a",)),
+                           ({"a": 1}, ("a", "b"))
+                           ])
+    
