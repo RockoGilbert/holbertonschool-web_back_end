4f9f9c788384b6044fe8a3522f2c8938a7b52260
RockoGilbert <evolve2122@gmail.com>
2022-07-12 18:24:59 -0500

    Fixing pycodestyle errors

--- 0x01-i18n/0-app.py
+++ 0x01-i18n/0-app.py
@@ -3,10 +3,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     """"Welcome HTML page"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
