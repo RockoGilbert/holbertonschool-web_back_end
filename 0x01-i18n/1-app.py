f4f7ea41987779d19504eb10e181958efe2094e7
RockoGilbert <evolve2122@gmail.com>
2022-07-12 18:23:19 -0500

    Rendering html

--- 0x01-i18n/1-app.py
+++ 0x01-i18n/1-app.py
@@ -6,13 +6,16 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """The configuration of the app"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en" 
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
