9cd1159ada85e90b11803266bbd4fcdc10fc81ab
RockoGilbert <evolve2122@gmail.com>
2022-07-18 14:03:23 -0500

    Setup file structure

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
 -- SQL script that creates a table users
+--  Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
 CREATE TABLE If NOT EXISTS `users` (  
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
