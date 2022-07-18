d21f7ef10c544bb9f8d43131792bc9e657299d8a
RockoGilbert <evolve2122@gmail.com>
2022-07-18 14:40:59 -0500

    A script that creates a table users

---
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users
+--  Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
+CREATE TABLE If NOT EXISTS `users` (  
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN')NOT NULL
+);
