f8dd0907f21fde483a96c37929e98fc790ee1081
RockoGilbert <evolve2122@gmail.com>
2022-07-25 12:37:57 -0500

    A program that displays a message

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-// Display proper message to end user
-
-process.stdout.write('Welcome to Holberton School, what is your name?\'n);
-
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is : ${name}`);
+  if (name) process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) process.exit();
+  else {
+    console.log('This important software is now closing');
+    process.exit();
   }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
 });
