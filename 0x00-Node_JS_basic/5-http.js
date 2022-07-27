6f1b0873486756a9035570c74ec781f9df1b6656
RockoGilbert <evolve2122@gmail.com>
2022-07-27 11:19:19 -0500

    Crated a server using hTTP model

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,27 +1,48 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async.js');
-const fs = require('fs');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = process.argv[2];
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') 
     res.write('Hello Holberton School!');
-  } else if (req.url === '/students') {
+
+  if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      //  const data = fs.readFileSync('3-read_file_async.js', 'utf-8',);
-
-      countStudents('database.csv');
-
-      // console.log(data)
-      // res.end(`${students.join('\n')}`);
+      const returnValue = await countStudents(DATABASE);
+      // res.end(`${returnValue.students.join('\n')}`);
+      res.write(`Number of students: ${returnValue.students.length}\n`);
+      res.write(
+        `Number of students in CS: ${
+          returnValue.studentsByCS.length
+        }. List: ${returnValue.studentsByCS.join(", ")}\n`
+      );
+      res.write(
+        `Number of students in SWE: ${
+          returnValue.studentsBySWE.length
+        }. List: ${returnValue.studentsBySWE.join(", ")}`
+      );
     } catch (error) {
+      res.statusCode = 404;
       res.end(error.message);
     }
   }
-  res.statusCode = 404;
   res.end();
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  //   res.write(`Server running at http://${hostname}:${port}/`);
+});
+
 module.exports = app;
