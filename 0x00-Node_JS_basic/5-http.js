7305d4c68b93717913f00a2127c5b84d494354bf
RockoGilbert <evolve2122@gmail.com>
2022-07-27 00:00:13 -0500

    Used function from database

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,23 +1,27 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async.js');
+const fs = require('fs');
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello Holberton School!');
-  }
-
-  if (req.url === '/students') {
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      await countStudents(process.argv[2]);
-      console.log(countStudents(process.argv[2]));
-      res.end(`${countStudents(process.argv[2]).join('\n')}`);
+      //  const data = fs.readFileSync('3-read_file_async.js', 'utf-8',);
+       
+      countStudents('database.csv')
+
+      // console.log(data)
+      // res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   }
+  res.statusCode = 404;
   res.end();
 });
+
 app.listen(1245);
 module.exports = app;
