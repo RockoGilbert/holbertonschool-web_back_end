9e5ba17a493d19bb63711b8d9ecc542b667b81df
RockoGilbert <evolve2122@gmail.com>
2022-07-26 22:23:08 -0500

    A small HTTP server

--- 0x00-Node_JS_basic/6-http_express.js
+++ 0x00-Node_JS_basic/6-http_express.js
@@ -1,6 +1,7 @@
-// Express App 
+// Express App
 
-const express = require('express')
+const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
