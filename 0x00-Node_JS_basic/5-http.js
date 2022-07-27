eacbfdce13c95a4a17f8c7d3c62898d53c9b6d6e
RockoGilbert <evolve2122@gmail.com>
2022-07-26 22:26:43 -0500

    Create a small HTTP server using http module

---
+++ 0x00-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http =  require('http');
+const countStudents = require("./3-read_file_async.js");
+ 
+const app = http.createServer(async (req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello Holberton School!");
+  }
+
+  if (req.url === "/students") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("This is the list of our students\n");
+    try {
+      await countStudent(process.argv[2]);
+      console.log(countStudent(process.argv[2]));
+      res.end(`${countStudent(process.argv[2]).join('\n')}`)
+      
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
+});
+app.listen(1245);
+module.exports = app;
