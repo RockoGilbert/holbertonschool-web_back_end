edf03d4e8b97ab5d89bd52199145d7d4d96186fb
RockoGilbert <evolve2122@gmail.com>
2022-07-27 23:46:42 -0500

    Recreated a small http server using express

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -2,23 +2,30 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
+const database = process.argv[2];
 
-app
-.get('/', (req, res) => res.send('Hello Holberton School!'))
-.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  await countStudents(process.argv[2])
-    .then((data = > {
-      const fields = Object.keys(data);
-      const }))
-
-
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
-  } catch (error) {
-    res.send(`${title}${error.message}`);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
-app.listen(1245);
+
+app.get('/students', (req, res) => {
+  countStudents(database)
+    .then((data) => {
+      const { students, csStudents, sweStudents } = data;
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.statusCode = 200;
+      res.end();
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(port);
+
 module.exports = app;
