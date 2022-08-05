6dd13d5d98ac1f8e3627b340e06b21ab16a57b0b
RockoGilbert <evolve2122@gmail.com>
2022-08-05 10:02:28 -0500

    Set test functions

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,35 +1,23 @@
 const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
-describe('calculateNumber', () => {
-  it('checks the output', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    assert.equal(calculateNumber(3.7, 1), 5);
-    assert.equal(calculateNumber(3.7, 1.2), 5);
-  });
-  it('negative numbers', () => {
-    assert.equal(calculateNumber(-1, 1), 0);
-    assert.equal(calculateNumber(1, -1), 0);
-    assert.equal(calculateNumber(-1, -1), -2);
-  });
-});
+describe("Test Suite", () => {
+    it("checks if int is positive", () => {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
 
-describe('calculateNumber', () => {
-  it('checks the output', () => {
-  
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    assert.equal(calculateNumber(3.7, 1), 5);
-    assert.equal(calculateNumber(3.7, 1.2), 5);
-  });
-  it('negative numbers', () => {
-    assert.equal(calculateNumber(-1, 1), 0);
-    assert.equal(calculateNumber(1, -1), 0);
-    assert.equal(calculateNumber(-1, -1), -2);
-  });
+    it("checks if int are negative", () => {
+      assert.equal(calculateNumber(-1, -2), -3);
+      assert.equal(calculateNumber(-1.5, -2.5), -3);
+      assert.equal(calculateNumber(-1.5, -2), -3);
+      assert.equal(calculateNumber(-1, -2.5), -3);
+    });
+
+    it("check argument/TypeError", () => {
+        assert.throws(() => calculateNumber(NaN, "a"), {name: 'TypeError'});
+        assert.throws(() => calculateNumber(NaN, 3), {name: 'TypeError'});
+    });
 });
