edb513cd05490aac16396977dfe3f4e402d690e6
RockoGilbert <evolve2122@gmail.com>
2022-08-09 11:15:31 -0500

    testing api's

--- 0x01-unittests_in_js/6-payment_token.js
+++ 0x01-unittests_in_js/6-payment_token.js
@@ -1,9 +1,11 @@
 // The function will take an argument called success
 
-const getPaymentTokenFromAPI = async (success) => {
+const getPaymentTokenFromAPI = (success) => {
   if (success) {
-    return { data: 'Successful response from the API' };
+    return new Promise((resolve, reject) => {
+      resolve({ data: 'Successful response from the API' });
+    });
   }
-}
+};
 
 module.exports = getPaymentTokenFromAPI;
--- 0x01-unittests_in_js/6-payment_token.test.js
+++ 0x01-unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,12 @@
 // use the done callback to execute this test
-
-const func = require('./6-payment_token.js');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 const expect = require('chai').expect;
 
-
-describe('test suite - getPaymentTokenFromAPI()', () => {
-  it('test getPaymentTokenFromAPI(true)', (done) => {
-    func(true).then(result => {
-      expect(result.data).to.equal('Successful response from the API');
+describe('getPaymentTokenFromAPI', () => {
+  it('Tests that a new promise is returned', (done) => {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response.data).to.equal('Successful response from the API');
       done();
     });
-  })
+  });
 });
