be85d412a89de19fd134a101c7adb4a4923b1786
RockoGilbert <evolve2122@gmail.com>
2022-08-11 09:43:59 -0500

    test 1 2

--- 0x01-unittests_in_js/3-payment.test.js
+++ 0x01-unittests_in_js/3-payment.test.js
@@ -1,14 +1,15 @@
-const sendPaymentRequestToApi = require('./3-payment.js');
-const Utils = require('./utils.js');
-const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
-  it('Tests sendpaymentRequestToApi function', () => {
-    const spyCalculateNumber = sinon.spy(Utils, 'calculateNumber');
+  it('Make sure math for sendPaymentRequestToAPI is the same as calculateNumber', () => {
+    const spiedFunction = sinon.spy(Utils, 'calculateNumber');
+
     sendPaymentRequestToApi(100, 20);
-    expect(spyCalculateNumber.calledOnce).to.be.true;
-    expect(spyCalculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    spyCalculateNumber.restore();
+
+    expect(spiedFunction.calledWith('SUM', 100, 20)).to.be.true;
+    spiedFunction.restore();
   });
 });
