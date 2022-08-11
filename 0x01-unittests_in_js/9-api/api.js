c079ecc5a3c068404a4e246f890f547310deb1e2
RockoGilbert <evolve2122@gmail.com>
2022-08-11 13:47:12 -0500

    Adding a new endpoint

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -1,27 +1,37 @@
-const { expect } = require('chai');
-const request = require('request');
+const { response } = require('express');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-describe('API Test', () => {
-  it('Tests that GET returns correct code and results', (done) => {
-    request('http://localhost:7865/', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+const port = 7865;
 
-  it('Tests /cart/:id is working', (done) => {
-    request('http://localhost:7865/cart/1', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 1');
-      done();
-    });
-  });
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
 
-  it('Tests /cart/:id fails when given non-number', (done) => {
-    request('http://localhost:7865/cart/a', (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  const id = req.params.id;
+  res.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
   });
+});
+
+app.post('/login', (req, res) => {
+  res.send(`Welcome ${req.body.userName}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
--- 0x01-unittests_in_js/9-api/api.test.js
+++ 0x01-unittests_in_js/9-api/api.test.js
@@ -1,27 +1,65 @@
 const { expect } = require('chai');
 const request = require('request');
 
-describe('API Test', () => {
-  it('Tests that GET returns correct code and results', (done) => {
-    request('http://localhost:7865/', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+describe('Index page', () => {
+  it('request /', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.statusMessage).to.equal('OK');
+        expect(body).to.equals('Welcome to the payment system');
+        done();
+      }
     });
   });
 
-  it('Tests /cart/:id is working', (done) => {
-    request('http://localhost:7865/cart/1', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 1');
-      done();
+  it('request /cart/:id success', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.statusMessage).to.equal('OK');
+        expect(body).to.equals('Payment methods for cart 12');
+        done();
+      }
     });
   });
 
-  it('Tests /cart/:id fails when given non-number', (done) => {
-    request('http://localhost:7865/cart/a', (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      done();
+  it('request /cart/:id error', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      }
+    });
+  });
+
+  it('request /available_payments', (done) => {
+    request('http://localhost:7865/available_payments', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.eql({ payment_methods: { credit_cards: true, paypal: false } });
+        done();
+      }
+    });
+  });
+
+  it('request /login success', (done) => {
+    request.post({ uri: 'http://localhost:7865/login', method: 'POST', json: { userName: "Betty" } }, (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).equals("Welcome Betty")
+        done();
+      }
+    });
+  });
+
+  it('request /login undefined', (done) => {
+    request.post({ uri: 'http://localhost:7865/login', method: 'POST' }, (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).equals("Welcome undefined")
+        done();
+      }
     });
   });
 });
