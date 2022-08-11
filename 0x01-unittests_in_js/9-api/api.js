e1e277eb9d13a5d3df32f1b559408ae39fc1b5d2
RockoGilbert <evolve2122@gmail.com>
2022-08-11 14:33:24 -0500

    Added new test suite

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -1,37 +1,20 @@
-const { response } = require('express');
+// create an instance of express called app
+
 const express = require('express');
-const bodyParser = require('body-parser')
-
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-const port = 7865;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  const id = req.params.id;
-  res.send(`Payment methods for cart ${id}`);
-});
+app
+  .get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+  })
 
-app.get('/available_payments', (req, res) => {
-  const id = req.params.id;
-  res.send({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
+  .get('/cart/:id([0-9]*)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+
+  })
+  .listen(7865, () => {
+    console.log('API available on localhost port 7865');
   });
-});
 
-app.post('/login', (req, res) => {
-  res.send(`Welcome ${req.body.userName}`);
-});
-
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+module.exports = app;
--- 0x01-unittests_in_js/9-api/api.test.js
+++ 0x01-unittests_in_js/9-api/api.test.js
@@ -1,65 +1,32 @@
-const { expect } = require('chai');
-const request = require('request');
+// Create one suite for the index page:
 
-describe('Index page', () => {
-  it('request /', (done) => {
+const {expect} = require('chai');
+const request = require("request");
+
+
+describe('test - GET /api', () => {
+  it('Tests GET returns correct code and res', (done) => {
     request('http://localhost:7865', (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(200);
-        expect(response.statusMessage).to.equal('OK');
-        expect(body).to.equals('Welcome to the payment system');
-        done();
-      }
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('test - GET /api/cart/:id([0-9]*)', () => {
+  it('Tests if /cart/:id([0-9]*) is working', (done) => {
+    request('http://localhost:7865/cart/1', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 1');
+      done();
     });
   });
 
-  it('request /cart/:id success', (done) => {
-    request('http://localhost:7865/cart/12', (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(200);
-        expect(response.statusMessage).to.equal('OK');
-        expect(body).to.equals('Payment methods for cart 12');
-        done();
-      }
+  it('Tests /cart/:id fails when NaN', (done) => {
+    request('http://localhost:7865/cart/a', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
     });
-  });
-
-  it('request /cart/:id error', (done) => {
-    request('http://localhost:7865/cart/hello', (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      }
-    });
-  });
-
-  it('request /available_payments', (done) => {
-    request('http://localhost:7865/available_payments', (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(200);
-        expect(JSON.parse(body)).to.equal({ payment_methods: { credit_cards: true, paypal: false } });
-        done();
-      }
-    });
-  });
-
-  it('request /login success', (done) => {
-    request.post({ uri: 'http://localhost:7865/login', method: 'POST', json: { userName: "Betty" } }, (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(200);
-        expect(body).equals("Welcome Betty")
-        done();
-      }
-    });
-  });
-
-  it('request /login undefined', (done) => {
-    request.post({ uri: 'http://localhost:7865/login', method: 'POST' }, (error, response, body) => {
-      if (response) {
-        expect(response.statusCode).to.equal(200);
-        expect(body).equals("Welcome undefined")
-        done();
-      }
-    });
-  });
+  })
 });
